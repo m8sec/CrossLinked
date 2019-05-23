4a362c74ef8fbc8c49056e4e8d50de763d9748fc
m8r0wn <m8r0wn@protonmail.com>
2019-05-23 09:49:20 -0400

    Fix typo

--- crosslinked.py
+++ crosslinked.py
@@ -176,10 +176,10 @@ def main(args):
     if args.verbose:
         for id, data in found_names.items():
             print("[+] {}\t: {}".format(data['first'] + " " + data['last'], data['title']))
-    print("[+] {}.txt complete, {} unique names found!".format(args.outfile, len(found_names)))
+    print("[+] {} complete, {} unique names found!".format(args.outfile, len(found_names)))
 
 if __name__ == '__main__':
-    VERSION = "0.0.3"
+    VERSION = "0.0.4"
     args = argparse.ArgumentParser(description="", formatter_class=argparse.RawTextHelpFormatter, usage=argparse.SUPPRESS)
     args.add_argument('--debug', dest="debug", action='store_true',help=argparse.SUPPRESS)
     args.add_argument('-t', dest='timeout', type=int, default=25,help='Timeout [seconds] for search threads (Default: 25)')
