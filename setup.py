aeb1ae0a1810ae4840b79e5cee4af4aa4fc916e0
Mike <13889819+m8sec@users.noreply.github.com>
2022-09-20 08:05:23 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='crosslinked',
-    version='0.2.0',
+    version='0.2.1',
     author='m8sec',
     license='GPLv3',
     long_description=long_description,
